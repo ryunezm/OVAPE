b31cdfd747f17cb0a31b9ac211adabcbd61e777d
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-08-21 23:43:22 -0500

    Trying fixes.

--- src/main/java/ryunezm/ovape/config/SecurityConfig.java
+++ src/main/java/ryunezm/ovape/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ryunezm.ovape.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
