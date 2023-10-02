d058a9fcd3b421651dc0c55889ab5cd5659df967
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-10-02 12:45:51 -0500

    Adding security.

--- fronted/src/app/login/login.component.ts
+++ fronted/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ApiService } from "../services/api.service";
+import { User } from '../services/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.sass']
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
   username: string = '';
   password: string = '';
+  loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private apiService: ApiService, private router: Router,) {
     this.loginForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(5)]],
       password: ['', [Validators.required, Validators.minLength(8)]]
@@ -22,9 +25,15 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-
       const username = this.loginForm.value.username;
       const password = this.loginForm.value.password;
+
+      this.apiService.login(username, password).subscribe((user: User) => {
+        this.router.navigateByUrl('/home').then(r => '');
+      });
     }
   }
+
+  ngOnInit(): void {}
+
 }
