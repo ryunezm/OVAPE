385ff386628103ae74eee4f725be7f0b641af4de
ryunezm <61928963+ryunezm@users.noreply.github.com>
2023-11-15 22:35:45 -0500

    --> Fix Lesson.java

--- src/main/java/ryunezm/ovape/content/models/Lesson.java
+++ src/main/java/ryunezm/ovape/content/models/Lesson.java
@@ -1,4 +1,27 @@
 package ryunezm.ovape.content.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "lesson")
 public class Lesson {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "module_id")
+    private Module module;
+
 }
