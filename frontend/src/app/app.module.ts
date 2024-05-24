442b9b3c4ad77a1750624f22a2e175c5f1bc8234
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-05-24 09:43:40 -0500

    Upgrading to Angular 18

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -17,7 +17,7 @@ import {MatInputModule} from "@angular/material/input";
 import {ContactComponent} from './components/information/contact/contact.component';
 import {AboutusComponent} from './components/information/aboutus/aboutus.component';
 import {MatMenuModule} from "@angular/material/menu";
-import {HttpClientModule} from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import {MatSelectModule} from "@angular/material/select";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatSnackBarModule} from '@angular/material/snack-bar';
@@ -29,42 +29,35 @@ import {CardComponent} from './components/base/card/card.component';
 import {MatTooltipModule} from "@angular/material/tooltip";
 import {LeftpanelComponent} from './components/base/leftpanel/leftpanel.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HomeComponent,
-    FooterComponent,
-    HeaderComponent,
-    ContactComponent,
-    AboutusComponent,
-    CardComponent,
-    LeftpanelComponent,
-  ],
-  imports: [
-    MatButtonModule,
-    MatToolbarModule,
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    MatIconModule,
-    MatInputModule,
-    MatMenuModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatSnackBarModule,
-    MatCardModule,
-    MatListModule,
-    MatStepperModule,
-    NgOptimizedImage,
-    MatTooltipModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        RegisterComponent,
+        HomeComponent,
+        FooterComponent,
+        HeaderComponent,
+        ContactComponent,
+        AboutusComponent,
+        CardComponent,
+        LeftpanelComponent,
+    ],
+    bootstrap: [AppComponent], imports: [MatButtonModule,
+        MatToolbarModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatIconModule,
+        MatInputModule,
+        MatMenuModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatListModule,
+        MatStepperModule,
+        NgOptimizedImage,
+        MatTooltipModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
