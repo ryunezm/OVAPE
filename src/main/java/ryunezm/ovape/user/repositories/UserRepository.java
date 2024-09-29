0136aa39e2801d14f5bb66b24209074f8ff614a1
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-09-29 01:34:16 -0500

    Cleaning up

--- src/main/java/ryunezm/ovape/user/repositories/UserRepository.java
+++ src/main/java/ryunezm/ovape/user/repositories/UserRepository.java
@@ -5,7 +5,8 @@ import ryunezm.ovape.user.models.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository <User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
